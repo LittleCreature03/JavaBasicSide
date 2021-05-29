7b9d0b5d3059503dc11403aa701f3c7985cace40
LittleCreature03 <79300173+LittleCreature03@users.noreply.github.com>
2021-05-29 19:25:18 +0530

    Add files via upload

---
+++ arrays/ArrayModel1.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class ArrayModel1 {
+
+	public static void main(String args[])
+	{
+		int arr[]= {100,101,102};
+		for(int i=0;i<arr.length;i++)
+		{
+		System.out.println(arr[i]);
+	}
+}}
